8f9316c6de5093931e0f26b252dcd4e50cd6fba0
bniass <bniass@gmail.com>
2020-03-05 15:43:04 +0000

    creation de la classe role

---
+++ src/main/java/cours/jee/springindevops/model/Role.java
@@ -0,0 +1,24 @@
+package cours.jee.springindevops.model;
+
+import java.io.Serializable;
+
+public class Role implements Serializable {
+    private long id;
+    private String libelle;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+}
