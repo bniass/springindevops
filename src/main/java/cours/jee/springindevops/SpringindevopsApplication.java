ecaa0cc180e7576d00e182b4ed10fd714a2d62a1
Baye Niass <bniass@gmail.com>
2020-03-05 13:45:11 +0000

    initialisation du projet

---
+++ src/main/java/cours/jee/springindevops/SpringindevopsApplication.java
@@ -0,0 +1,13 @@
+package cours.jee.springindevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringindevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringindevopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cours/jee/springindevops/SpringindevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package cours.jee.springindevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringindevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
